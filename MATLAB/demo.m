ec48cd39da2f74455ec3d3fc3091e001d2a8beb8
Nikos Pitsianis <pitsianis@yahoo.com>
2020-08-18 22:13:49 -0400

    temporary demo check correctness

--- MATLAB/demo.m
+++ MATLAB/demo.m
@@ -11,10 +11,12 @@ close all
 
 
 %% PARAMETERS
-
-A = sprand(5,5,2/5);
+n = 500000; p = 4;
+A = sprand(n,n,p/n);
 A = sparse( logical( A+A' ) );
 A = A - diag(diag(A));
+
+% load('data/testGraph06.mat') % it crashes with this input!
 
 %% (BEGIN)
 
@@ -28,6 +30,34 @@ F = fglt( A );
 
 fprintf( '   - DONE in %.2f sec\n', toc(ticFGlT) );
 
+%% check against ORCA
+
+G = graph.runOrca(A); 
+
+fprintf('Discrepancy %f \n', norm(G - F, 'fro'))
+
+% [ii,jj] = find(F ~= G);
+% 
+% ii = unique(ii);
+% jj = unique(jj);
+% 
+% fprintf('Diferences in %d verices %d graphlets\n', ...
+%   length(ii), length(jj))
+% 
+% fprintf('FGLT\n')
+% util.dispmat(F(ii(1:min(10,length(ii))),:),'%5d')
+% 
+% fprintf('ORCA\n')
+% util.dispmat(G(ii(1:min(10,length(ii))),:),'%5d')
+% 
+% D = F - G;
+% fprintf('diff\n')
+% util.dispmat(D(ii(1:min(10,length(ii))),:),'%5d')
+
+[Draw,Dnet] = graph.graphletFreqFastV2(A);
+
+fprintf('Discrepancy FGLT raw %f \n', norm(Draw - F, 'fro'))
+fprintf('Discrepancy ORCA net %f \n', norm(G - Dnet, 'fro'))
 
 %% (END)
 
