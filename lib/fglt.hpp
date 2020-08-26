f94f5d34c8530ca50f139b8bfc276da4e67c14f6
Jason Barmparesos <jason@jasonb.dev>
2020-08-26 12:06:53 +0300

    Document the public functions

--- lib/fglt.hpp
+++ lib/fglt.hpp
@@ -34,8 +34,24 @@ typedef size_t    mwIndex;        /* unsigned pointer-width integer */
 extern "C" {
 #endif
 
+/*!
+ * \brief Get the number of parallel workers available.
+ *
+ * \return The number of workers available. Always 1 when built without Cilk support.
+ */
 int getWorkers();
 
+/*!
+ * \brief Perform the FGLT transform.
+ *
+ * \param f [out] An array-of-pointers of size (n, 16) where the raw frequencies should be stored.
+ * \param fn [out] An array-of-pointers of size (n, 16) where the net frequencies should be stored.
+ * \param ii [in] The column indices of the adjacency matrix.
+ * \param jStart [in] The first non-zero row index of each column.
+ * \param n [in] The number of columns of the adjacency matrix.
+ * \param m [in] The number of edges in the graph.
+ * \param np [in] The number of parallel workers to use for the transform.
+ */
 void compute
   (
    double ** const f,
