b3901655af057e7bcce97ff2a7e63d9055b4c828
Dimitris Floros <flojim4@gmail.com>
2020-08-30 22:28:45 +0300

    Simplified MATLAB demos

--- MATLAB/demo.m
+++ MATLAB/demo.m
@@ -10,12 +10,8 @@ clear
 close all
 
 %% PARAMETERS
-n = 1000000; p = 3;
-A = sprand(n,n,p/n);
-A = sparse( logical( A+A' ) );
-A = A - diag(diag(A));
 
-% load('data/testGraph06.mat') 
+listDataFile = dir('data/*.mat');
 
 %% (BEGIN)
 
@@ -25,41 +21,30 @@ fprintf('\n *** begin %s ***\n\n',mfilename);
 
 ticFGlT = tic; fprintf( '...compute graphlets...\n' ); 
 
-Fnet = fglt( A );
+for iData = 1:length( listDataFile )
+
+  structData = load( ['data' filesep listDataFile(iData).name] );
+  Fnet = fglt( structData.A );
+  
+  figure(1)
+  plot(graph(structData.A))
+  title( listDataFile(iData).name )
+  
+  fprintf([repmat('%2d ',1,size(Fnet,2)) '\n'],Fnet');
+  
+  pause
+  
+end
+
 
 fprintf( '   - DONE in %.2f sec\n', toc(ticFGlT) );
 
-%% check against ORCA
-
-G = graph.runOrca(A); 
-
-fprintf('Discrepancy %f\n', norm(G - Fnet, 'fro'))
-
-% [ii,jj] = find(F ~= G);
-% 
-% ii = unique(ii);
-% jj = unique(jj);
-% 
-% fprintf('Differences in %d verices %d graphlets\n', ...
-%   length(ii), length(jj))
-% 
-% fprintf('FGLT\n')
-% util.dispmat(F(ii(1:min(10,length(ii))),:),'%5d')
-% 
-% fprintf('ORCA\n')
-% util.dispmat(G(ii(1:min(10,length(ii))),:),'%5d')
-% 
-% D = F - G;
-% fprintf('diff\n')
-% util.dispmat(D(ii(1:min(10,length(ii))),:),'%5d')
-
-% [Draw,Dnet] = graph.graphletFreqFastV2(A);
-% fprintf('Discrepancy FGLT raw %f \n', norm(Draw - Fraw, 'fro'))
-% fprintf('Discrepancy ORCA net %f \n', norm(G - Fnet, 'fro'))
 
 %% (END)
 
 fprintf('\n *** end %s ***\n\n',mfilename);
+
+
 
 
 %%------------------------------------------------------------
@@ -72,10 +57,13 @@ fprintf('\n *** end %s ***\n\n',mfilename);
 %
 % CHANGELOG 
 %
+%   1.0 (Aug 30, 2020) - Dimitris
+%       * added multiple datasets, looping through each
+% 
 %   0.1 (Aug 18, 2020) - Dimitris
 %       * initial implementation
 %
-% TIMESTAMP     <Aug 19, 2020: 12:31:15 Dimitris>
+% TIMESTAMP     <Aug 30, 2020: 22:26:28 Dimitris>
 %
 % ------------------------------------------------------------
 
