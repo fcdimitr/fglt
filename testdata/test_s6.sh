9fd61c2d3d4ffa06f22a3694bd045c36b2960e44
Alexandros-Stavros Iliopoulos <1577182+ailiop@users.noreply.github.com>
2021-03-26 09:33:05 -0400

    Fix test-script assumption of fglt binary location
    I.e.: ensure that the test scripts can be run regardless of where they
    are called from (particularly useful if one does not build/install FGlT
    in a 1-deep subdirectory of the source tree).

--- testdata/test_s6.sh
+++ testdata/test_s6.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+DIR_TESTDATA=$(cd "$(dirname "$0")" && pwd)
+
 FGLT=${1:-./fglt}
 
-${FGLT} ../testdata/s6.mtx
-diff freq_net.csv ../testdata/s6_freq_net_gold.csv
+${FGLT} "${DIR_TESTDATA}"/s6.mtx
+diff freq_net.csv "${DIR_TESTDATA}"/s6_freq_net_gold.csv
